68f95639c0becec7d1091932ab6b9c410ca97149
juditAldeguer <jud.vicens@hotmail.es>
2022-02-03 18:22:16 +0100

    feat: add new SimpleCounter component

---
+++ src/components/SimpleCounter.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>Contador sencillo</h2>
+  <h3>{{ counterComputed }}</h3>
+  <button @click="decrement">Decrementar</button>
+  <button @click="increment">Incrementar</button>
+</template>
+
+<script>
+import { ref, computed } from 'vue'
+
+export default {
+  name: 'Simple Counter',
+  setup () {
+    const counter = ref(0)
+
+    const counterComputed = computed(() => {
+      const result = `El valor de counter es ${counter.value}`
+      return result
+    })
+    const increment = () => {
+      counter.value++
+    }
+    const decrement = () => {
+      counter.value--
+    }
+    return { counterComputed, increment, decrement }
+  }
+}
+</script>
+
+<style lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
     <HelloWorld />
+    <hr />
+    <SimpleCounter />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import SimpleCounter from '../components/SimpleCounter.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    SimpleCounter
   }
 }
 </script>
