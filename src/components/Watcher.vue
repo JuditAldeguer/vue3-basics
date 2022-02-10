c0be80b237f43a3bfb0bd765062ab3cefa4dea4b
juditAldeguer <jud.vicens@hotmail.es>
2022-02-10 09:47:23 +0100

    style: header each components & add component Watcher

---
+++ src/components/Watcher.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>Watcher</h2>
+</template>
+
+<script>
+export default {
+  name: 'Watcher'
+}
+</script>
+
+<style lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,8 @@
     <SimpleCounter />
     <hr />
     <ReactiveCounter />
+    <hr />
+    <Watcher />
   </div>
 </template>
 
@@ -13,13 +15,22 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 import SimpleCounter from '../components/SimpleCounter.vue'
 import ReactiveCounter from '../components/ReactiveCounterr.vue'
+import Watcher from '../components/Watcher.vue'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
     SimpleCounter,
-    ReactiveCounter
+    ReactiveCounter,
+    Watcher
   }
 }
 </script>
+
+<style>
+h2 {
+  color: rgb(33, 179, 94);
+  padding-top: 1rem;
+}
+</style>
