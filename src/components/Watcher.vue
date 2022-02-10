993111afd63195bf99deed42994e4d731f63529e
juditAldeguer <jud.vicens@hotmail.es>
2022-02-10 10:22:26 +0100

    feat: add watch(counter)

--- src/components/Watcher.vue
+++ src/components/Watcher.vue
@@ -1,10 +1,37 @@
 <template>
   <h2>Watcher</h2>
+  <h3>{{counter}}</h3>
+  <h3>{{message}}</h3>
+  <button @click="decrement">Decrementar</button>
+  <button @click="increment">Incrementar</button>
 </template>
 
 <script>
+import { ref, watch } from 'vue'
+
 export default {
-  name: 'Watcher'
+  name: 'Watcher',
+  setup () {
+    const counter = ref(0)
+    const message = ref('')
+
+    watch(counter, () => {
+      if (counter.value === 5) {
+        message.value = 'El valor de counter es 5'
+      } else {
+        message.value = ''
+      }
+    })
+
+    const increment = () => {
+      counter.value++
+    }
+    const decrement = () => {
+      counter.value--
+    }
+
+    return { counter, message, increment, decrement }
+  }
 }
 </script>
 
