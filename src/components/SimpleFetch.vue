3ab5362ebfe108ad1d203443df578460779e6e12
juditAldeguer <jud.vicens@hotmail.es>
2022-02-10 15:54:11 +0100

    feat: fetch onMounted

--- src/components/SimpleFetch.vue
+++ src/components/SimpleFetch.vue
@@ -1,11 +1,37 @@
 <template>
-<h2>SimpleFetch</h2>
+  <h2>SimpleFetch</h2>
+  <ul v-if="users">
+    <li v-for="user in users" :key="user.id">
+      {{ user.name }} - {{ user.email }}
+    </li>
+  </ul>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue'
 export default {
-  name: 'SimpleFetch'
+  name: 'SimpleFetch',
+  setup () {
+    const users = ref(null)
+    onMounted(async () => {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+      )
+      users.value = await response.json()
+    })
+    return { users }
+  }
 }
 </script>
 
-<style lang='scss'></style>
+<style>
+ul {
+  list-style-type: none;
+  padding-bottom: 2rem;
+  width: fit-content;
+  margin: 0 auto;
+}
+li {
+  border: 1px solid black;
+}
+</style>
